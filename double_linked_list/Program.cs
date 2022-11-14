0a07354fe4dc945546411a383a3d9aa2cd01ed85
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-11-14 17:51:49 +0800

    create class node and give the comment in line

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -2,7 +2,15 @@
 
 namespace double_linked_list
 {
-    class node { }
+    class node
+    {
+        /*Node class represent the node of doubly linked list.
+        *it consist of the information part and links to
+        *its succeeding and preceeding 
+        *in terms of next and previous */
+        public int noMhs;
+        
+    }
     internal class program
     {
 
