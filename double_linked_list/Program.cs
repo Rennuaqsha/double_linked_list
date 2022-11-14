73dfd701fa4e4aa9e774ab9b6582ac95a4cd39db
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-11-14 18:00:52 +0800

    create method add node and add no mhs, nim  and name

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -25,6 +25,19 @@ namespace double_linked_list
         {
             START = null;
         }
+
+        public void addnode()
+        {
+            int nim;
+            string nm;
+            Console.Write("\nEnter the roll number off the student: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of student: ");
+            nm = Console.ReadLine();
+            node newNode = new node();
+            newNode.noMhs = nim;
+            newNode.name = nm;
+        }
     }
     
 }
