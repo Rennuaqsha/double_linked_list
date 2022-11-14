1bf13b0ecf3ad42bf396c6be30987c7d476d8853
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-11-14 18:09:19 +0800

    add if return to check the list is the empty

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -37,7 +37,22 @@ namespace double_linked_list
             node newNode = new node();
             newNode.noMhs = nim;
             newNode.name = nm;
+
+            //check if the list empty 
+            if (START == null || nim <= START.noMhs)
+            {
+                if ((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nDuplicate Number not allowed");
+                    return;
+                }
+                newNode.next = START;
+                if (START != null)
+                    START.prev = newNode;
+                newNode.prev = null;
+                return;
+            }
         }
+
     }
-    
 }
